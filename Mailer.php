64c4927f510765158787e2b7c7fe3f6d9db5242a
Michael Macaulay <56690114+MichaelMacaulay@users.noreply.github.com>
2020-09-17 14:12:37 -0400

    Add files via upload

--- Mailer.php
+++ Mailer.php
@@ -1,19 +1,13 @@
 <?php
-
 // Create simple message & email
 $email_to = 'michaelmacaulay33@gmail.com';
 $subject = 'Here is your download!';
-$message = "You will be a programming pro in no time! \n\n Thanks so much for joining the email list.";
-
-
+$message = "You will be a programming pro in no time! Thanks so much for joining the email list.";
 // Create the email headers including the from
 $headers = "From: TechTime <techtimedev@gmail.com.me>";
-
 // Send the email to the user
     if ( mail($email_to, $subject, $message, $headers) )
         echo 'success';
     else
         echo 'not sent';
-
-
 ?>
