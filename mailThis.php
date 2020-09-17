6c3f2651affece5bf78bda8d51c813872045f816
Michael Macaulay <56690114+MichaelMacaulay@users.noreply.github.com>
2020-09-17 14:20:00 -0400

    Add files via upload

--- mailThis.php
+++ mailThis.php
@@ -3,9 +3,13 @@
 // $_POST["name"]; = 'userName';
 // Also need to add person's name to the email message. Do it later
 
-$subject = 'Here is your download!';
-$message = "Hey You will be a programming pro in no time! Thanks so much for joining the email list.";
-$headers = "From: <techtimedev@gmail.com";
+$subject = 'HELLO WORLD';
+$message = "DOWNLOAD THIS NOW";
+$headers = array(
+    'From' => 'techtimedev@gmail.com',
+    'Reply-To' => 'techtimedev@gmail.com',
+    'X-Mailer' => 'PHP/' . phpversion()
+);
 
 
 // Call the mail function & pass in the to email, subject, message and headers/from email
