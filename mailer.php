a47f752b560c0f2dbfd3619508028fbb20652daa
Michael Macaulay <56690114+MichaelMacaulay@users.noreply.github.com>
2020-09-17 14:28:41 -0400

    Add files via upload

--- mailer.php
+++ mailer.php
@@ -1,8 +1,8 @@
 <html>
 <body>
 
-Welcome <?php echo $_POST["name"]; ?><br>
-Your email address is: <?php echo $_POST["email"]; ?>
+Welcome <?php echo $_GET["name"]; ?><br>
+Your email address is: <?php echo $_GET["email"]; ?>
 
 </body>
 </html>
